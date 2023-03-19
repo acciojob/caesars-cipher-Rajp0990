60b598f2380907956d01bab543f9fb14ab063d15
Rajp0990 <106186423+Rajp0990@users.noreply.github.com>
2023-03-19 13:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
@@ -13,7 +11,12 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+	const string = encodedStr;
+	const arr = [];
+ for(let i = 0;i<string.length;i++){
+	string.charAt(i) = string.charAt(i+13);
+	 arr.push(string[i]);
+ }
   return ;//return decodedArr
 }
 
